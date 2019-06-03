c58ac462090f001cbca42b955404ab0c10601d12
Min Khant Kyaw <minkhantkyaw.na@gmail.com>
2019-06-03 11:54:29 -0400

    updating

---
+++ app.js
@@ -0,0 +1,97 @@
+let pressToStartP = document.getElementById('press-to-start');
+let currentWordP = document.getElementById('current-word');
+let letterGuessedP = document.getElementById('letters-guessed');
+let chanceLeftP = document.getElementById('chance-left');
+let resultP = document.getElementById('result');
+let startCheck = 0;
+let check = 0;
+let aToZ = [];
+let letterGuessed = [];
+let chanceLeft = 10;
+let newWordsArr = [];
+let randomWords = ['backenddeveloper', 'frontenddeveloper', 'webdeveloper', 'programmer', 'engineer', 'photographer', 'reactdeveloper'];
+let random = Math.floor(Math.random() *randomWords.length);
+let randomWord = randomWords[random].split('');
+let displayWord = randomWord.map(x => '_');
+for(let i = 97; i < 123; i++) {
+    aToZ.push(i);
+}
+let alphabets = aToZ.map(x => String.fromCharCode(x));
+
+function animation() {
+    currentWordP.classList = "letter-spacing correct-animation";
+    setTimeout(() => {
+        currentWordP.classList = "letter-spacing";
+    }, 500);
+}
+
+document.onkeyup = function(e) {
+    if (e.key !== 'Meta') {
+        check = 1;
+    }
+    let alphaCheck = 0;
+    if(alphabets.indexOf(e.key) !== -1) {
+        alphaCheck = 1;
+    }
+    if(check === 1 && startCheck === 1 && alphaCheck === 1) {
+        if(randomWord.indexOf(e.key) !== -1) {
+            let index = randomWord.indexOf(e.key);
+            displayWord[index] = e.key;
+            currentWordP.textContent = `'${displayWord.join('')}'`;
+            animation();
+            randomWord[index] = '_';
+            
+        } else {
+            chanceLeft--;
+            letterGuessed.push(e.key.toUpperCase());
+            letterGuessedP.textContent = `'${letterGuessed}'`;
+            chanceLeftP.textContent = chanceLeft;
+        }
+        function result() {
+            if(chanceLeft === 0) {
+                resultP.textContent = 'You lost!';
+                setTimeout(() => {
+                    let confirm = window.confirm('You lost!!! Do you try to guess new words?');
+                    if(confirm) {   
+                        alert(`The current word is "${randomWords[random]}"`);
+                        random = Math.floor(Math.random() *randomWords.length);
+                        randomWord = randomWords[random].split('');
+                        displayWord = randomWord.map(x => '_');
+                        chanceLeft = 10;
+                        letterGuessed = [];
+                        currentWordP.textContent = `'${displayWord.join('')}'`;
+                        letterGuessedP.textContent = '';
+                        chanceLeftP.textContent = chanceLeft;
+                        resultP.textContent = '';
+                    }
+                },1);   
+            } else if(displayWord.indexOf('_') === -1) {
+                resultP.textContent = 'You Win!';
+                setTimeout(() => {
+                    let confirm = window.confirm('Do you want to guess new words?');
+                    if(confirm) {
+                        random = Math.floor(Math.random() *randomWords.length);
+                        randomWord = randomWords[random].split('');
+                        displayWord = randomWord.map(x => '_');
+                        chanceLeft = 10;
+                        letterGuessed = [];
+                        currentWordP.textContent = `'${displayWord.join('')}'`;
+                        letterGuessedP.textContent = '';
+                        resultP.textContent = '';
+                        chanceLeftP.textContent = chanceLeft;
+                        letterGuessedP.textContent = '';
+                    }
+                },10);      
+            }
+        }
+        result();
+    }
+    if(e.key !== "Meta") {
+        startCheck = 1;
+        pressToStartP.textContent = '';
+        pressToStartP.classList = '';
+        currentWordP.textContent = `'${displayWord.join('')}'`;
+        chanceLeftP.textContent = chanceLeft;
+    }
+    
+}
